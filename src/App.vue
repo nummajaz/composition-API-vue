4e0be6cd88d9a2694af2c476de96f6bd7365b2ae
unknown <noorjazuli@gmail.com>
2022-09-12 07:51:23 +0700

    perbandigan ref dan reactive

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,37 @@
 
   <div>
 
-    Ini adalah angka {{number}}
+    Halo nama saya adalah {{state.user.name}} <br>
+    status saya adalah {{state.loading}}
+
   </div>
   
 </template>
 
 <script>
-import {ref } from 'vue';
+import {reactive} from 'vue';
 
 export default {
   setup(){
-    const number = ref(0)
-  
-    setInterval( () => {
+    const state = reactive({
+      loading : false,
+      user: {
+        name: "jazuli",
+        umur: 25
+      },
+      info:{
+        status : "wiraswasta",
+        hobi: "turu"
+      }
+
+    })
+    setTimeout(() =>{
+      state.user.name  = "Nur Muhammad Jazuli",
+      state.loading = true
+    }, 2000)
     
-      number.value++
-    }, 500)
-    return {
-      number
+    return{
+      state
     }
   }
 
