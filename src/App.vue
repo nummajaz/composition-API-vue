517ddbebcc3e15db8822aa9fc54a974eeb499f64
unknown <noorjazuli@gmail.com>
2022-09-12 08:12:12 +0700

    menggunakan methode & helpers

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
 
   <div>
+    <button @click="decrement">-</button>
+    {{number}}
 
-    Halo nama saya adalah {{name}}
+    <button @click="increment">+</button>
   </div>
   
 </template>
 
 <script>
 import {ref } from 'vue';
+import useCounter from './helpers/count';
 
 export default {
   setup(){
-    const name = ref("Jazuli")
-  
-    setTimeout( () => {
-    
-      name.value = "Muhammad"
 
-    }, 2000)
-    return {
-      name
-    }
+    const number = ref(0)
+
+    const {increment, decrement} = useCounter(number)
+    return{increment, number, decrement}
   }
 
 }
