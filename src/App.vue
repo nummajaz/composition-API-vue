bfbb12099fb02571ddd121c5a2bab4527b24e463
unknown <noorjazuli@gmail.com>
2022-09-12 09:02:41 +0700

    lifecycle vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,19 @@
 <template>
 
   <div>
-    <button @click="decrement">-</button>
-    {{number}}
-
-    <button @click="increment">+</button>
+    <PostS/>
   </div>
   
 </template>
 
 <script>
-import {ref } from 'vue';
-import useCounter from './helpers/count';
+
+import PostS from './components/PostS';
 
 export default {
-  setup(){
-
-    const number = ref(0)
-
-    const {increment, decrement} = useCounter(number)
-    return{increment, number, decrement}
-  }
-
+  components:{
+    PostS
+}
 }
 </script>
 
